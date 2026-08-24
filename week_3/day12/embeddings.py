71873b4d7a5fa5dc18c00167f8c4c923e706b1b5
Dev-L66 <sabeebashil@yahoo.com>
2026-08-24 16:49:22 +0300

    embeddings

---
+++ week_3/day12/embeddings.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+
+
+
+def cosnie_similarity(a,b):
+    return np.dot(a,b)/(np.linalg.norm(a) * np.linalg.norm(b))
+
+model = SentenceTransformer("all-MiniLM-L6-v2") # 384
+
+text = "Machine lerning is fun."
+
+
+embedding = model.encode(text)
+
+# print(embedding.shape)
+# print(embedding)
+# print(embedding[:10])
+
+
+t1 = "There are 24 paid leaves"
+t2="There are 24 vacation days"
+
+
+v1 = model.encode(t1)
+v2 = model.encode(t2)
+
+
+print(cosnie_similarity(v1, v2))
